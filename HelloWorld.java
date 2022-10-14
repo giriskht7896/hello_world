b0d7b30a21bf67a824f160ff675623358915cb5d
giriskht7896 <110625367+giriskht7896@users.noreply.github.com>
2022-10-14 11:24:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World , how are you , have a nice day");
+    System.out.println("Hello, World , how are you ,  nice day");
   }
 }
